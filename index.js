437f76a6d760775853267e4804d93e63f6cf5865
jincdream <275727449@qq.com>
2015-05-09 15:59:29 +0800

    add one
    add one

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function (content, file, conf) {
+
+  if (file.isHtmlLike) {
+    // return content = content.replace(/[Uu][Tt][Ff]-*8/g, 'gbk');
+    return content.replace(/(<meta.*?)utf-8(.*?>)/,'$1gbk$2')
+  }
+
+};
