6b0c4be9bf7a557f573918e7e0e35c99e263611e
jincdream <275727449@qq.com>
2015-05-12 23:35:36 +0800

    over
    over

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 'use strict';
 
 module.exports = function (content, file, conf) {
-  console.log(file)
-  if (file.isHtmlLike) {
+  // if (file.isHtmlLike) {
     // return content = content.replace(/[Uu][Tt][Ff]-*8/g, 'gbk');
-    return content.replace(/(<meta.*?)utf-8(.*?>)/gi,'$1gbk$2')
-  }
 
+    return content.replace(/(<meta.*?)utf-8(.*?>)/gi,'$1gbk$2')
+  // }
 };
